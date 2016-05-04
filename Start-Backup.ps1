99558cd4e62d0604bf444971d321e2b8a9296ab9
tuxgoose <1phillip.guzman@gmail.com>
2016-05-04 09:53:52 -0500

    Added files via upload

---
+++ Start-Backup.ps1
@@ -0,0 +1,88 @@
+﻿       <# ----------------------------------------------------------------------------
+
+ Original Author: Phillip Guzman
+
+   Creation Date: 03/03/2014
+
+   Script Function: Repairs DBErrors in Mozy Enterprise
+
+   Modified By:
+
+   Revision History:
+
+
+#>
+[CmdLetBinding()]
+    Param(
+    [Parameter(Mandatory=$True,HelpMessage="Please Enter Compputer Name(s)")]
+              [string[]]$Computers
+          )
+
+#reads admin input
+cls
+
+
+
+
+#$StringName= $ComputerToFixName
+
+
+
+
+#Allows you to start processe's on Remote Machines
+Function New-RemoteProcess {
+    Param([string]$computername=$env:computername,
+        [string]$cmd=$(Throw "You must enter the full path to the command which will create the process.")
+    )
+
+    $ErrorActionPreference="SilentlyContinue"
+
+    Trap {
+        Write-Warning "There was an error connecting to the remote computer or creating the process"
+        Continue
+    }    
+
+    Write-Output "Connecting to $computername" -ForegroundColor CYAN
+    Write-Output "Process to create is $cmd" -ForegroundColor CYAN
+
+    [wmiclass]$wmi="\\$computername\root\cimv2:win32_process"
+
+    #bail out if the object didn't get created
+    if (!$wmi) {return}
+
+    $remote=$wmi.Create($cmd)
+
+    if ($remote.returnvalue -eq 0) {
+        Write-Output "Successfully launched $cmd on $computername with a process id of" $remote.processid -ForegroundColor GREEN
+    }
+    else {
+        Write-Output "Failed to launch $cmd on $computername. ReturnValue is" $remote.ReturnValue -ForegroundColor RED
+    }
+}
+
+
+
+
+foreach($i in $Computers)
+{
+
+$Computers=$i
+
+
+
+
+function repair{
+(Get-WmiObject -ComputerName $Computers -Class win32_computersystem).name
+
+
+Set-Service -Name MozyEnterprisebackup -ComputerName $Computers -Status Running
+New-RemoteProcess -computername $Computers -cmd "C:\Program Files\MozyEnterprise\MozyEnterprisestat.exe"
+New-RemoteProcess -computername $Computers -cmd "C:\Program Files\MozyEnterprise\mozyenterpriseutil.exe /backup"
+Write-Output  "$Computers Service has now changed" -ForegroundColor magenta
+}
+
+repair
+& C:\Users\phil.guzman\Desktop\CMTrace.exe "\\$Computers\C$\Program Files\MozyEnterprise\data\MozyEnterprise.log"
+#Uptime
+}
+
---
+++ WorkFlowOutPutOnline.ps1
@@ -0,0 +1,107 @@
+﻿
+
+Workflow Cycle{
+$list = get-content C:\Mozy\dberror.txt
+foreach -parallel($i in $list)
+{
+
+
+
+if(test-connection $i -count 1 -ErrorAction SilentlyContinue)
+{
+if(Get-WmiObject win32_computersystem)
+{
+#[string[]]$computers += $i
+Write-Output "$i" | Out-file -filepath "C:\Mozy\Online.txt" -append
+}
+}
+}
+}
+
+
+
+#Allows you to start processe's on Remote Machines
+Function New-RemoteProcess {
+    Param([string]$computername=$env:computername,
+        [string]$cmd=$(Throw "You must enter the full path to the command which will create the process.")
+    )
+
+    $ErrorActionPreference="SilentlyContinue"
+
+    Trap {
+        Write-Warning "There was an error connecting to the remote computer or creating the process"
+        Continue
+    }    
+
+    Write-Host "Connecting to $computername" -ForegroundColor CYAN
+    Write-Host "Process to create is $cmd" -ForegroundColor CYAN
+
+    [wmiclass]$wmi="\\$computername\root\cimv2:win32_process"
+
+    #bail out if the object didn't get created
+    if (!$wmi) {return}
+
+    $remote=$wmi.Create($cmd)
+
+    if ($remote.returnvalue -eq 0) {
+        Write-Host "Successfully launched $cmd on $computername with a process id of" $remote.processid -ForegroundColor GREEN
+    }
+    else {
+        Write-Host "Failed to launch $cmd on $computername. ReturnValue is" $remote.ReturnValue -ForegroundColor RED
+    }
+}
+Function OpenAll{
+    $list = Get-Content C:\Mozy\Online.txt
+    foreach($i in $list)
+    {
+    sleep -Seconds 60
+
+    & C:\Users\phil.guzman\Desktop\CMTrace.exe "\\$i\C$\Program Files\MozyEnterprise\data\MozyEnterprise.log"
+    }
+
+}
+
+#File that has a list of currupted Mozy Computers
+
+Function Cycle2{
+$list = Get-Content C:\Mozy\Online.txt
+foreach($i in $list)
+{
+
+Write-Host $i
+
+
+Set-Service -Name MozyEnterprisebackup -ComputerName $i -Status Stopped 
+Invoke-Command -ComputerName $i -ScriptBlock {Stop-Process -Name mozy* -Force}
+
+Start-Sleep -s 2
+Remove-Item -path "\\$i\C$\Program Files\MozyEnterprise\Data\state.dat" 
+
+
+Set-Service -Name MozyEnterprisebackup -ComputerName $i -Status Running
+
+New-RemoteProcess -computername $i -cmd "C:\Program Files\MozyEnterprise\mozyenterpriseutil.exe /backup"
+Write-Host  "$i Service has now changed" -ForegroundColor magenta
+
+
+
+}
+
+
+}
+
+function LastReboot{
+$list = Get-Content C:\Mozy\Online.txt
+
+foreach($i in $list)
+{
+
+Write-warning $i
+Get-CimInstance win32_operatingsystem -ComputerName $i | select  LastBootUpTime | ft -AutoSize
+
+}
+}
+Cycle
+#OpenAll
+#Cycle2
+#LastReboot
